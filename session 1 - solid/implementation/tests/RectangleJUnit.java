9873beae5f9928d1afd262fb6887327b37ff5ad4
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-10-11 22:01:40 +0330

    change Height added

--- session 1 - solid/implementation/tests/RectangleJUnit.java
+++ session 1 - solid/implementation/tests/RectangleJUnit.java
@@ -13,10 +13,17 @@ public class RectangleJUnit {
     }
 
     @Test
-    public void changeParameter() {
+    public void changeWidth() {
         Shape rectangle = new Rectangle(3, 4);
         rectangle.setWidth(5);
         Assert.assertEquals(5, rectangle.getWidth());
+    }
+
+    @Test
+    public void changeHeight() {
+        Shape rectangle = new Rectangle(3, 4);
+        rectangle.setHeight(6);
+        Assert.assertEquals(6, rectangle.getHeight());
     }
 }
 
