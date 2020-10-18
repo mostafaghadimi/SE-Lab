c4b31c3929d4dfbc4c6d698ac3ebdb53b80a9697
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-10-18 23:09:26 +0330

    [session2] implementation completed

--- session 2 - decorator&bridge/bridge/src/test/PowerTest.java
+++ session 2 - decorator&bridge/bridge/src/test/PowerTest.java
@@ -1,5 +1,7 @@
 package test;
+import main.impl.mult.MultImplRecursive;
 import main.impl.mult.MultImplStandard;
+import main.impl.power.PowerImplRecursive;
 import main.impl.power.PowerImplStandard;
 import main.interfaces.Power;
 import org.junit.Test;
@@ -7,8 +9,26 @@ import org.junit.Assert;
 
 public class PowerTest {
     @Test
-    public void testPower() {
+    public void testPowerStandardRecursive() {
+        Power power = new PowerImplStandard(new MultImplRecursive());
+        Assert.assertEquals(81, power.power(3, 4), 0);
+    }
+
+    @Test
+    public void testPowerRecursiveRecursive() {
+        Power power = new PowerImplRecursive(new MultImplRecursive());
+        Assert.assertEquals(81, power.power(3, 4), 0);
+    }
+
+    @Test
+    public void testPowerRecursiveStandard(){
+        Power power = new PowerImplRecursive(new MultImplStandard());
+        Assert.assertEquals(81, power.power(3, 4), 0);
+    }
+
+    @Test
+    public void testPowerStandardStandard(){
         Power power = new PowerImplStandard(new MultImplStandard());
-        Assert.assertEquals(12, power.power(3, 4), 0);
+        Assert.assertEquals(81, power.power(3, 4), 0);
     }
 }
