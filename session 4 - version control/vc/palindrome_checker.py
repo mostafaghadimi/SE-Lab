71a79db1bfcc7c47c9ba1c2bc581728ccdde2946
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-11-01 19:23:22 +0330

    [session 4] palindrome_checker function added

--- session 4 - version control/vc/palindrome_checker.py
+++ session 4 - version control/vc/palindrome_checker.py
@@ -7,5 +7,12 @@ def digit_counter(number):
         number /= 10
     return counter
 
+def palindrome_checker(number):
+    digit_no = digit_counter(number)
+    for i in range((digit_no + 1) // 2):
+        if ((number // 10 ** (i)) % 10  != (number // 10 **(digit_no - i)) % 10):
+            return False
+        
+    return True
 
 number = int(input())
