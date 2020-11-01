67a3f510f73e5b4ad654cce6464ffa8edd690893
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-11-01 19:19:26 +0330

    [session 4] digit_counter function added

--- session 4 - version control/vc/palindrome_checker.py
+++ session 4 - version control/vc/palindrome_checker.py
@@ -1,5 +1,10 @@
-#to be completed
-
-
+def digit_counter(number):
+    counter = 0
+    while number > 0:
+        if number < 1:
+            break
+        counter += 1
+        number /= 10
+    return counter
 
 number = int(input())
